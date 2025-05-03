07478899cfb50e686d9178eb0428bd5995130fb0
Harshath MohanaSundaram <96179007+HarshathMohanaSundaram@users.noreply.github.com>
2025-05-03 09:50:28 +0530

    Create the Container for Calculator

---
+++ src/components/CalculatorContainer.tsx
@@ -0,0 +1,11 @@
+import '../style/common.css'
+
+const CalculatorContainer = () => {
+  return (
+    <div className='container'>
+      <h1>Simple Calculator</h1>
+    </div>
+  );
+} 
+
+export default CalculatorContainer;
